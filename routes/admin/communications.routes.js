5aa5a6dae6e3446d9499c3269013a748547e7ccb
john <shayojohnraphael@gmail.com>
2026-07-14 17:23:04 +0300

    controler fix and middleware

--- routes/admin/communications.routes.js
+++ routes/admin/communications.routes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const CommunicationsController = require('../../controllers/admin/CommunicationsController');
-const { verifyAdmin } = require('../../middleware/verifyAdmin');
+const verifyAdmin = require('../../middleware/verifyAdmin');
 
 // Use auth middleware on all routes
 router.use(verifyAdmin);
